aee03fc8191c01ec557942bad9ee6dd2eec9a939
Azhar <ahmedazhar110011@gmail.com>
2021-07-08 22:17:26 +0530

    Completed Day12 THA

--- Day12FE/script.js
+++ Day12FE/script.js
@@ -1,35 +1,55 @@
-let addDayBtn = document.getElementById("add-day")
+let main = document.getElementById("main")
 let addTaskBtn = document.getElementById("add-task")
-let lower = document.getElementById("lower")
-let modal = document.getElementById("modal")
-let taskModal = document.getElementById("add-task-modal")
-let confirmDay = document.getElementById("date-ok")
-let confirmTask = document.getElementById("task-ok")
+let addTaskModal = document.getElementById("add-task-modal")
+let taskDesc = document.getElementById("input-task")
+let registerTask = document.getElementById("reg-task-btn")
+
+let value = taskDesc.value
+console.log(taskDesc.value)
+let count = 1
+let store = []
+let taskTemplate = function(id, task,status){
+    return {
+        id,
+        task,
+        status
+    }
+}
+
+function openModal(){
+    addTaskModal.classList.toggle("invisible")
+}
+
+function displayTask(){
+    let div = document.createElement('div')
+    div.classList.add('task')
+    let currTask = taskTemplate(count, taskDesc.value, true)
+    store.push(currTask)
+    console.log(store)
+    div.innerHTML = `<span>${count}. ${currTask.task}</span><button class = "cmplt-task-btn" id = ${count}>&#10003</button>`
+    main.appendChild(div);
+    (function addListener(){
+        let cmpltBtn = document.getElementById(`${count}`)
+        cmpltBtn.addEventListener("click",()=>{
+                    let completedTask = cmpltBtn.previousElementSibling.innerHTML
+                    cmpltBtn.parentElement.innerHTML = `<strike> ${completedTask}</strike>`
+                    console.log("adeed")
+                })
+    })();
+}
 
 
-addDayBtn.addEventListener('click', ()=>{
-    modal.classList.toggle("invisible")
-    document.body.classList.toggle("semi-transparent")
-    lower.classList.add("invisible")
-    console.log('day added')
-})
-
-confirmDay.addEventListener("click",()=>{
-    modal.classList.toggle("invisible")
-    document.body.classList.toggle("semi-transparent")
-    lower.classList.remove("invisible")
-})
+if(taskDesc.value !== null || taskDesc.value !== null ){
+    registerTask.addEventListener('click',()=>{
+        addTaskModal.classList.toggle("invisible")
+        displayTask()
+        count += 1
+    })
+}
 
 addTaskBtn.addEventListener('click', ()=>{
-    taskModal.classList.toggle("invisible")
-    document.body.classList.toggle("semi-transparent")
-    upper.classList.add("invisible")
-    lower.classList.add("invisible")
+    openModal()
 })
 
-confirmTask.addEventListener("click", ()=>{
-    taskModal.classList.toggle("invisible")
-    document.body.classList.toggle("semi-transparent")
-    upper.classList.remove("invisible")
-    lower.classList.remove("invisible")
-})
+
+
